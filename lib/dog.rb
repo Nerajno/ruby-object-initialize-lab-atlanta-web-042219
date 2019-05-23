8f1836db9c5f348e9f20ef099208a817627eabb5
github username <github email address>
2019-05-23 13:59:16 -0400

    round_2

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,28 @@
 class Dog
-     def initialize(name,breed="Mutt")
-        @name = name
-        @breed = breed
-     end
 
-     def name=(name)
-        @name = name
-     end
+   attr_accessor :name
+   
+   def initialize(name, breed="Mutt")
+      @name = name
+      @breed = breed
+   end
 
-     def name
-        @name
-     end
+   def name=(name)
+      @name = name
+   end
 
-    
-     def breed=(breed)            
-        @breed = breed
-     end
+   def name 
+      @name
+   end
 
-     def breed
-        @breed
-     end
+   def breed= (breed)
+      @breed= breed
+   end
+
+   def breed 
+      @breed
+   end
+
+
 end
+
